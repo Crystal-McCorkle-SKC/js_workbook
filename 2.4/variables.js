b52e08cdc739b36b47076653c6b498b3800762be
CLMcCork <37164206+CLMcCork@users.noreply.github.com>
2020-10-22 14:25:42 -0500

    Created files and completed 2.3-2.4

---
+++ 2.4/variables.js
@@ -0,0 +1,13 @@
+//working with variables
+"use strict";
+let name = "John"; 
+let admin = name;
+alert (admin);
+
+//giving the right name
+let ourPlanetName = "earth";
+let currentUserName = "Mary"; 
+
+//Uppercase const?
+const BIRTHDAY = "18.04.1982"; //Yes, make uppercase b/c your bday is always the same/will never change
+const AGE = someCode(BIRTHDAY); //No, should be lowercase b/c age always increases each year, so it's not constant 
